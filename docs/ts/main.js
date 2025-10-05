6e869d7d1e1fdb808f66baebcda835f29cfe2d52
DataPure <puredata.in@gmail.com>
2025-10-05 17:51:03 +0530

    Delete docs directory

--- docs/ts/main.js
+++
@@ -1,4 +0,0 @@
-// Compiled JavaScript
-var welcomeMessage = "Hello from TypeScript!";
-console.log(welcomeMessage);
-alert(welcomeMessage);
--- docs/ts/main.ts
+++
@@ -1,4 +0,0 @@
-// This is a simple TypeScript example
-const welcomeMessage: string = "Hello from TypeScript!";
-console.log(welcomeMessage);
-alert(welcomeMessage);
